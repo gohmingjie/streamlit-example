f0bf867181ab4b95bcf5b6e821b94cae6cec3903
gohmingjie <111936150+gohmingjie@users.noreply.github.com>
2022-08-24 10:11:45 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -4,7 +4,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-st.header("Sabrina Goh- my first application"
+st.header("Sabrina Goh- my first application")
 # Simple Iris Flower Prediction App
 
 This app predicts the **Iris flower** type!
